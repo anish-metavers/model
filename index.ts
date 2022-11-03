146117d3fb00b20f54cb612a69275b98fb4608cf
sid-metaversity <siddharth@metaversitytechnologies.com>
2022-11-03 16:29:01 +0530

    Updated

--- index.ts
+++ index.ts
@@ -11,55 +11,47 @@ import Bets from './bets';
 import UserLog from './userLog';
 
 const DATABASE = async () => {
-    const isProd = process.env.IS_PROD === 'true';
-    const sequelize = new Sequelize(
-        isProd ? process.env.PROD_MYSQL_DB : process.env.LOCAL_MYSQL_DB,
-        isProd ? process.env.PROD_MYSQL_USER : process.env.LOCAL_MYSQL_USER,
-        isProd
-            ? process.env.PROD_MYSQL_PASSWORD
-            : process.env.LOCAL_MYSQL_PASSWORD,
-        {
-            host: isProd
-                ? process.env.PROD_MYSQL_HOST
-                : process.env.LOCAL_MYSQL_HOST,
-            dialect: 'mysql',
-            logging: false,
-            dialectOptions: {
-                dateStrings: true,
-                typeCast: true,
-            },
-            timezone: '+05:30',
-        },
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_ROOT,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.HOST,
+      dialect: 'mysql',
+      logging: false,
+      dialectOptions: {
+        dateStrings: true,
+        typeCast: true,
+      },
+      timezone: '+05:30',
+    },
+  );
+
+  try {
+    await sequelize.authenticate();
+    console.log(
+      'Database connection established successfully!!........................................',
     );
+    const db = {
+      sequelize: sequelize,
+      User: User(sequelize, DataTypes),
+      Role: Role(sequelize, DataTypes),
+      UserRole: UserRole(sequelize, DataTypes),
+      Permission: Permission(sequelize, DataTypes),
+      RolePermission: RolePermission(sequelize, DataTypes),
+      TempUser: TempUser(sequelize, DataTypes),
+      SportBet: SportBet(sequelize, DataTypes),
+      Wallet: Wallet(sequelize, DataTypes),
+      Bets: Bets(sequelize, DataTypes),
+      UserLog: UserLog(sequelize, DataTypes),
+    };
 
-    try {
-        await sequelize.authenticate();
-        console.log(
-            'Database connection established successfully!!........................................',
-        );
-        const db = {
-            sequelize: sequelize,
-            User: User(sequelize, DataTypes),
-            Role: Role(sequelize, DataTypes),
-            UserRole: UserRole(sequelize, DataTypes),
-            Permission: Permission(sequelize, DataTypes),
-            RolePermission: RolePermission(sequelize, DataTypes),
-            TempUser: TempUser(sequelize, DataTypes),
-            SportBet: SportBet(sequelize, DataTypes),
-            Wallet: Wallet(sequelize, DataTypes),
-            Bets: Bets(sequelize, DataTypes),
-            UserLog: UserLog(sequelize, DataTypes),
-        };
+    await sequelize.sync({ force: true });
 
-        // await sequelize.sync({ alter: true });
-
-        global.DB = db;
-    } catch (error) {
-        console.error(
-            'Unable to connect to database!!!!!!!!!!!!!!!!!!!!!!',
-            error,
-        );
-    }
+    global.DB = db;
+  } catch (error) {
+    console.error('Unable to connect to database!!!!!!!!!!!!!!!!!!!!!!', error);
+  }
 };
 
 export default DATABASE;
