a26fc9b344bfb51151277782e81a06ebf7d04af4
sid-metaversity <siddharth@metaversitytechnologies.com>
2022-11-09 10:34:36 +0530

    Formatted

--- index.ts
+++ index.ts
@@ -13,54 +13,54 @@ import ThirdPartyBet from './thirdPartyBet';
 import Config from './config';
 
 const DATABASE = async () => {
-    const sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_ROOT,
-        process.env.DB_PASSWORD,
-        {
-            host: process.env.HOST,
-            dialect: 'mysql',
-            logging: false,
-            dialectOptions: {
-                dateStrings: true,
-                typeCast: true,
-            },
-            timezone: '+05:30',
-        },
-    );
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_ROOT,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.HOST,
+      dialect: 'mysql',
+      logging: false,
+      dialectOptions: {
+        dateStrings: true,
+        typeCast: true,
+      },
+      timezone: '+05:30',
+    },
+  );
 
-    try {
-        await sequelize.authenticate();
-        console.log('---- !!MySQL DB Connected Successfully!! ----');
-        const db = {
-            sequelize: sequelize,
-            User: User(sequelize),
-            Role: Role(sequelize),
-            UserRole: UserRole(sequelize),
-            Permission: Permission(sequelize),
-            RolePermission: RolePermission(sequelize),
-            TempUser: TempUser(sequelize),
-            SportBet: SportBet(sequelize),
-            Wallet: Wallet(sequelize),
-            Bets: Bets(sequelize),
-            UserLog: UserLog(sequelize),
-            Config: Config(sequelize),
-            ThirdPartyBet: ThirdPartyBet(sequelize),
-        };
+  try {
+    await sequelize.authenticate();
+    console.log('---- !!MySQL DB Connected Successfully!! ----');
+    const db = {
+      sequelize: sequelize,
+      User: User(sequelize),
+      Role: Role(sequelize),
+      UserRole: UserRole(sequelize),
+      Permission: Permission(sequelize),
+      RolePermission: RolePermission(sequelize),
+      TempUser: TempUser(sequelize),
+      SportBet: SportBet(sequelize),
+      Wallet: Wallet(sequelize),
+      Bets: Bets(sequelize),
+      UserLog: UserLog(sequelize),
+      Config: Config(sequelize),
+      ThirdPartyBet: ThirdPartyBet(sequelize),
+    };
 
-        // Setting the association of model
-        Object.keys(db).forEach((modelName) => {
-            if (db[modelName].associate) {
-                db[modelName].associate(db);
-            }
-        });
+    // Setting the association of model
+    Object.keys(db).forEach((modelName) => {
+      if (db[modelName].associate) {
+        db[modelName].associate(db);
+      }
+    });
 
-        await sequelize.sync({ alter: true });
+    // await sequelize.sync({ alter: true });
 
-        global.DB = db;
-    } catch (error) {
-        console.error('## Unable to connect to database ##', error);
-    }
+    global.DB = db;
+  } catch (error) {
+    console.error('## Unable to connect to database ##', error);
+  }
 };
 
 export default DATABASE;
